4b74cba839a4e133a5027f76da8f0aa629aa1009
Victor Dantas <victorr.dantas@gmail.com>
2023-09-18 11:36:40 -0400

    [200~

--- src/main/java/com/victordantas/workshopmongo/resources/UserResource.java
+++ src/main/java/com/victordantas/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.victordantas.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victordantas.workshopmongo.domain.User;
+import com.victordantas.workshopmongo.dto.UserDTO;
 import com.victordantas.workshopmongo.services.UserService;
 
 @RestController
@@ -19,9 +21,12 @@ public class UserResource {
 	private UserService userService;
 	
 	@RequestMapping(method = RequestMethod.GET) // ou @GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>>findAll() {
+		
 		List<User> list = userService.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+		
 	}
 	
 }
