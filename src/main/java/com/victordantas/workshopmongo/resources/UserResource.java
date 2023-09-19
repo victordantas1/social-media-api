6edd2bde5a3b34d1d2f3002fbd3d5a0f3f5606a0
Victor Dantas <victorr.dantas@gmail.com>
2023-09-19 13:48:38 -0400

    Deleting user with DELETE

--- src/main/java/com/victordantas/workshopmongo/resources/UserResource.java
+++ src/main/java/com/victordantas/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,7 +36,7 @@ public class UserResource {
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<UserDTO>findById(@PathVariable String id) {
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		
 		UserDTO user = new UserDTO(userService.findById(id));
 		return ResponseEntity.ok().body(user);
@@ -50,6 +51,13 @@ public class UserResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+		
 	}
 	
 }
