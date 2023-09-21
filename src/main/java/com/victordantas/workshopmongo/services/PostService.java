13cd4d57a3175cae38c6275c706f172efae6165f
Victor Dantas <victorr.dantas@gmail.com>
2023-09-21 11:02:02 -0400

    Adding comments to posts / Getting a post by id

---
+++ src/main/java/com/victordantas/workshopmongo/services/PostService.java
@@ -0,0 +1,28 @@
+package com.victordantas.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victordantas.workshopmongo.domain.Post;
+import com.victordantas.workshopmongo.repository.PostRepository;
+import com.victordantas.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public List<Post> findAll() {
+		return postRepository.findAll();
+	}
+	
+	public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}
