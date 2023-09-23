c9be25c1d686792372b04669d75714f1dae1067e
Victor Dantas <victorr.dantas@gmail.com>
2023-09-23 12:55:40 -0400

    Simple query with query methods

--- src/main/java/com/victordantas/workshopmongo/resources/PostResource.java
+++ src/main/java/com/victordantas/workshopmongo/resources/PostResource.java
@@ -7,9 +7,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victordantas.workshopmongo.domain.Post;
+import com.victordantas.workshopmongo.resources.util.URL;
 import com.victordantas.workshopmongo.services.PostService;
 
 @RestController
@@ -21,17 +23,20 @@ public class PostResource {
 	
 	@GetMapping
 	public ResponseEntity<List<Post>>findAll() {
-		
 		List<Post> list = postService.findAll();
 		return ResponseEntity.ok().body(list);
-		
 	}
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> findById(@PathVariable String id) {
-		
 		Post post = postService.findById(id);
 		return ResponseEntity.ok().body(post);
-		
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> posts = postService.findByTitle(text);
+		return ResponseEntity.ok().body(posts);
 	}
 }
--- src/main/java/com/victordantas/workshopmongo/services/PostService.java
+++ src/main/java/com/victordantas/workshopmongo/services/PostService.java
@@ -25,4 +25,8 @@ public class PostService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public List<Post> findByTitle(String text) {
+		return postRepository.findByTitleContainingIgnoreCase(text);
+	}
+	
 }
