d0b0271f7666c8c7020669958a71a24c02d31ff1
Victor Dantas <victorr.dantas@gmail.com>
2023-09-23 15:56:04 -0400

    Consultation with several criteria

--- src/main/java/com/victordantas/workshopmongo/services/PostService.java
+++ src/main/java/com/victordantas/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.victordantas.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,4 +30,9 @@ public class PostService {
 		return postRepository.searchTitle(text);
 	}
 	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
+		
+	}
 }
